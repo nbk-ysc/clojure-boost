ab03f878f3ff0beb0a0d8abab40fff7fecd012b9
Filipe Monteiro <filipe.monteiro@nubank.com.br>
2022-07-21 11:23:46 -0300

    conclui alguns exercicios

--- src/clojure_boost/compras.clj
+++ src/clojure_boost/compras.clj
@@ -1,5 +1,8 @@
 (ns clojure-boost.compras
-  (:use clojure.pprint))
+  (:use clojure.pprint)
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
+            [ultra-csv.core :as csv]))
 
 (defn nova-compra
   "Retorna uma nova compra"
@@ -13,141 +16,10 @@
                 }])
 
 (defn lista-compras []
-  "Retorna todas as compras realizadas"
-    [{
-      :data "2022-01-01"
-      :valor 129.90
-      :estabelecimento "Outback"
-      :categoria "Alimentação"
-      :cartao 1234123412341234
-      }
-     {
-      :data "2022-01-02"
-      :valor 260.00
-      :estabelecimento "Dentista"
-      :categoria "Saúde"
-      :cartao 1234123412341234
-      }
-     {
-      :data "2022-02-01"
-      :valor 20.00
-      :estabelecimento "Cinema"
-      :categoria "Lazer"
-      :cartao 1234123412341234
-      }
-     {
-      :data "2022-01-10"
-      :valor 150.00
-      :estabelecimento "Show"
-      :categoria "Lazer"
-      :cartao 4321432143214321
-      }
-     {
-      :data "2022-02-10"
-      :valor 289.99
-      :estabelecimento "Posto de gasolina"
-      :categoria "Automóvel"
-      :cartao 4321432143214321
-      }
-     {
-      :data "2022-02-20"
-      :valor 79.90
-      :estabelecimento "iFood"
-      :categoria "Alimentação"
-      :cartao 4321432143214321
-      }
-     {
-      :data "2022-03-01"
-      :valor 85.00
-      :estabelecimento "Alura"
-      :categoria "Educação"
-      :cartao 4321432143214321
-      }
-     {
-      :data "2022-01-30"
-      :valor 85.00
-      :estabelecimento "Alura"
-      :categoria "Educação"
-      :cartao 1598159815981598
-      }
-     {
-      :data "2022-01-31"
-      :valor 350.00
-      :estabelecimento "Tok&Stok"
-      :categoria "Casa"
-      :cartao 1598159815981598
-      }
-     {
-      :data "2022-02-01"
-      :valor 400.00
-      :estabelecimento "Leroy Merlin"
-      :categoria "Casa"
-      :cartao 1598159815981598
-      }
-     {
-      :data "2022-03-01"
-      :valor 50.00
-      :estabelecimento "Madero"
-      :categoria "Alimentação"
-      :cartao 6655665566556655
-      }
-     {
-      :data "2022-03-01"
-      :valor 70.00
-      :estabelecimento "Teatro"
-      :categoria "Lazer"
-      :cartao 6655665566556655
-      }
-     {
-      :data "2022-03-04"
-      :valor 250.00
-      :estabelecimento "Hospital"
-      :categoria "Saúde"
-      :cartao 6655665566556655
-      }
-     {
-      :data "2022-04-10"
-      :valor 130.0
-      :estabelecimento "Drogaria"
-      :categoria "Saúde"
-      :cartao 6655665566556655
-      }
-     {
-      :data "2022-03-10"
-      :valor 100.0
-      :estabelecimento "Show de pagode"
-      :categoria "Lazer"
-      :cartao 3939393939393939
-      }
-     {
-      :data "2022-03-11"
-      :valor 25.9
-      :estabelecimento "Dogão"
-      :categoria "Alimentação"
-      :cartao 3939393939393939
-      }
-     {
-      :data "2022-03-12"
-      :valor 215.87
-      :estabelecimento "Praia"
-      :categoria "Lazer"
-      :cartao 3939393939393939
-      }
-     {
-      :data "2022-04-01"
-      :valor 976.88
-      :estabelecimento "Oficina"
-      :categoria "Automóvel"
-      :cartao 3939393939393939
-      }
-     {
-      :data "2022-04-01"
-      :valor 85.0
-      :estabelecimento "Alura"
-      :categoria "Educação"
-      :cartao 3939393939393939
-      }
-     ])
+  "retorna lista via csv"
+  (csv/read-csv "compras.csv"
+                {:field-names [:data :valor :estabelecimento :categoria :cartao]}))
+
 (defn filtra-cartao
   "Filtra cartão e gera os valores para outra função"
   [cartao lista-compras]
@@ -177,8 +49,7 @@
   (-> data
       (str/split #"-")
       (second)
-      (Integer/parseInt)
-      ))
+      (Integer/parseInt)))
 
 (defn lista-compras-mes
   "Retorna a lista de compras a partir de um mês"
@@ -186,7 +57,7 @@
   (->> lista-compras
        (filter #(= mes (filtra-mes (:data %))))))
 
-(println "Lista de compras por mês" (lista-compras-mes (lista-compras) 01))
+(println "Lista de compras por mês" (lista-compras-mes (lista-compras) 04))
 
 (defn total-gasto-no-mes
   [lista-compras mes]
@@ -200,19 +71,16 @@
 (defn agrupar-categoria
   [lista-compras]
   (->> lista-compras
-       (group-by :categoria)
-       vec))
+       (group-by :categoria)))
 
 (defn lista-valores [[categoria valor]]
-  {
-   :categoria categoria
-   :valor (reduce + (map :valor valor))
-   }
-  )
+  [categoria (reduce + (map :valor valor))])
 
 (defn lista-todas-categorias
   [lista-compras]
-  (map lista-valores (agrupar-categoria lista-compras)))
+  (into {} (map lista-valores (agrupar-categoria lista-compras))))
 
 (println "Valor somado de todas as categorias")
 (pprint (lista-todas-categorias (lista-compras)))
+
+
