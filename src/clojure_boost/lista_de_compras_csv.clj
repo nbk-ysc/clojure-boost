738cf610acbfece8d47e6ac526725d4ae6a87d7c
vitorcardosodossantos <vitor.cardoso@easynvest.com.br>
2022-07-24 20:15:04 -0300

    Create files about lista de compras using map and csv

---
+++ src/clojure_boost/lista_de_compras_csv.clj
@@ -0,0 +1,6 @@
+(ns clojure-boost.lista-de-compras-csv
+(:require [ultra-csv.core :as ultra]))
+
+(defn lista-compras []
+  (ultra/read-csv "compras.csv" {:field-names [:data :valor :estabelecimento :categoria :cartao]}))
+(println (lista-compras))
