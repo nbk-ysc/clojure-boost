1f1d7895ba002d69cccd221f7aa6ebde0095f587
Arthur Teixeira <arthurteixeira444@gmail.com>
2022-12-12 10:31:38 -0300

    feat: Creating lista-compras function

--- src/clojure_boost/funcoes.clj
+++ src/clojure_boost/funcoes.clj
@@ -1,9 +1,23 @@
-(ns clojure-boost.funcoes)
+(ns clojure-boost.funcoes
+  (:require clojure.java.io
+            clojure.data.csv))
 
 (defn nova-compra
-  [data valor estabelecimento categoria cartao]
+  [[data valor estabelecimento categoria cartao]]
   {:data data
    :valor valor
    :estabelecimento estabelecimento
    :categoria categoria
    :cartao cartao})
+
+(defn lista-compras
+  []
+  (let [dados-das-compras
+        (into []
+              (with-open [reader (clojure.java.io/reader "compras.csv")]
+                (doall (clojure.data.csv/read-csv reader))))]
+
+    (->> dados-das-compras
+         (rest)
+         (map nova-compra)
+         (into []))))
