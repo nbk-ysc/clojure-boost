57b00431d4b29b329ba5427df01027d78cf0e5b9
Arthur Teixeira <arthurteixeira444@gmail.com>
2022-12-19 15:17:33 -0300

    feat: Creating insere-compra! function

--- src/clojure_boost/banco.clj
+++ src/clojure_boost/banco.clj
@@ -8,5 +8,8 @@
 (defn insere-compra [compra vetor-de-compras]
   (let [id-gerado (+ (count vetor-de-compras) 1)
         compra-com-id (assoc compra :id id-gerado)]
-
     (conj vetor-de-compras compra-com-id)))
+
+(defn insere-compra!
+  [compra vetor-de-compras]
+  (swap! vetor-de-compras #(insere-compra %2 %1) compra))
