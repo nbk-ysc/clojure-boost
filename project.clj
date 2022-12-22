617be4dd7bfcfbfb4e231224be1dfafb7c64abc4
Arthur Teixeira <arthurteixeira444@gmail.com>
2022-12-22 15:55:35 -0300

    build: Adding the plumatic schema library

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/data.csv "1.0.1"]]
+                 [org.clojure/data.csv "1.0.1"]
+                 [prismatic/schema "1.4.1"]]
   :repl-options {:init-ns clojure-boost.core})
