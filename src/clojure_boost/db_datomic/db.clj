652d7abf887b511530c2ff3d2de6366e5e9ab756
Arthur Teixeira <arthurteixeira444@gmail.com>
2023-01-06 11:06:47 -0300

    feat: Creating functions apaga-banco!, cria-schema!, insere-compra! and the schema symbol

--- src/clojure_boost/db_datomic/db.clj
+++ src/clojure_boost/db_datomic/db.clj
@@ -7,3 +7,40 @@
   []
   (d/create-database db-uri)
   (d/connect db-uri))
+
+(defn apaga-banco!
+  []
+  (d/delete-database db-uri))
+
+(def schema [{:db/ident       :compra/id
+              :db/valueType   :db.type/uuid
+              :db/cardinality :db.cardinality/one
+              :db/unique      :db.unique/identity}
+
+             {:db/ident       :compra/data
+              :db/valueType   :db.type/string
+              :db/cardinality :db.cardinality/one}
+
+             {:db/ident       :compra/valor
+              :db/valueType   :db.type/bigdec
+              :db/cardinality :db.cardinality/one}
+
+             {:db/ident       :compra/estabelecimento
+              :db/valueType   :db.type/string
+              :db/cardinality :db.cardinality/one}
+
+             {:db/ident       :compra/categoria
+              :db/valueType   :db.type/string
+              :db/cardinality :db.cardinality/many}
+
+             {:db/ident       :compra/cartao
+              :db/valueType   :db.type/long
+              :db/cardinality :db.cardinality/one}])
+
+(defn cria-schema!
+  [conn]
+  (d/transact conn schema))
+
+(defn insere-compra!
+  [db compra]
+  (d/transact db compra))
