59a689dc7e58e3276351521a33d18eccebd286a3
John Soares <john.soares@john.soares>
2023-01-10 02:18:44 -0300

    Datomic  cria-conexao! e apaga-banco!

--- src/clojure_boost/semana6.clj
+++ src/clojure_boost/semana6.clj
@@ -1,9 +1,12 @@
-(ns clojure-boost.semana6 (:require [datomic.api :as d]))
+(ns clojure-boost.semana6 (:require
+                            [datomic.api :as d]))
+
+
+(def db-uri "datomic:dev://localhost:4334/clojure-boost")
 
 (defn cria-conexao! []
-  (let [db-uri "datomic:dev://localhost:4334/clojure-boost"]
-    (d/create-database db-uri)
-    (d/connect db-uri)))
+  (d/create-database db-uri)
+  (d/connect db-uri))
 
 (println (cria-conexao!))
 "Uma mensagem semelhante a de abaixo deve aparecer se tudo estiver correto:
@@ -12,4 +15,7 @@
 {:unsent-updates-queue 0, :pending-txes 0,
 :next-t 1000, :basis-t 66, :index-rev 0,
 :db-id teste-81f05d8b-94f1-4e73-9e74-e15c8513aa4c}"
+
+(defn apaga-banco! []
+  (d/delete-database db-uri))
 
